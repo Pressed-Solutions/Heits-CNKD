40153cadfb2df30365131ec11a22fbf2d3d50433
Andrew Minion <andrew@andrewrminion.com>
2014-09-30 09:00:51 -0400

    add styling hooks for job dashboard

--- job-dashboard-page.php
+++ job-dashboard-page.php
@@ -13,17 +13,22 @@
  */
 
 get_header(); ?>
+
 <div id="card"></div>
-<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+<div class="col12">
+    <div class="pad">
+    <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
-				<h1><?php the_title(); ?></h1>
-				<?php if ( is_user_logged_in() ) { echo '<p><a href="/job-dashboard/">Back to all jobs</a></p>'; } ?>
-				<?php the_content(); ?>
-				<?php wp_link_pages( array( 'before' => '' . __( 'Pages:', 'boilerplate' ), 'after' => '' ) ); ?>
-				<?php edit_post_link( __( 'Edit', 'boilerplate' ), '', '' ); ?>
+                    <h1><?php the_title(); ?></h1>
+                    <?php if ( is_user_logged_in() ) { echo '<p><a href="/job-dashboard/">Back to all jobs</a></p>'; } ?>
+                    <?php the_content(); ?>
+                    <?php wp_link_pages( array( 'before' => '' . __( 'Pages:', 'boilerplate' ), 'after' => '' ) ); ?>
+                    <?php edit_post_link( __( 'Edit', 'boilerplate' ), '', '' ); ?>
 
-				<?php comments_template( '', true ); ?>
+                    <?php comments_template( '', true ); ?>
 
-<?php endwhile; ?>
+    <?php endwhile; ?>
+    </div><!-- .pad -->
+</div><!-- .col12 -->
 
 <?php get_footer(); ?>
